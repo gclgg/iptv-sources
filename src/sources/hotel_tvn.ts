238cc915808ce047f56dee01ad08925e31b10267
yunnysunny <yunnysunny@gmail.com>
2026-03-20 10:40:23 +0800

    feat: update hotel-tvn m3u link

--- src/sources/hotel_tvn.ts
+++ src/sources/hotel_tvn.ts
@@ -4,7 +4,7 @@ export const hotel_tvn_sources: TSources = [
   {
     name: 'whyun-pages/hotel-tvn',
     f_name: 'hotel_tvn',
-    url: 'https://raw.githubusercontent.com/whyun-pages/hotel-tvn/refs/heads/main/lives.m3u',
+    url: 'https://hotel-tvn.pages.dev/lives.m3u',
     filter: default_m3u_filter,
   },
 ];
